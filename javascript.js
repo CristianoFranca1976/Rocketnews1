2cb39e0e2a8f4371022c6f2d983021fbfd0522d3
Cristiano Ferro De Franca <135919856+CristianoFranca1976@users.noreply.github.com>
2023-10-24 18:09:44 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function mensagem() {
+  document.getElementById("enviado").value;
+  enviado.style.display = "flex";
+  enviado.style.justifyContent = "center";
+  enviado.style.alignItems = "center";
+}
+
+function resetPagina() {
+  location.reload();
+}
+
+function returnPage() {
+    window.location.href = 'index.html';
+}
